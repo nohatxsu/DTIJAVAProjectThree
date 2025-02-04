66274c6d791c1547814e312993f019bc8f42549d
Wichakorn <s6752410002@sau.ac.th>
2025-02-04 14:36:40 +0700

    v.1 start study method

--- src/com/sau/dti/method/MyClassMet01.java
+++ src/com/sau/dti/method/MyClassMet01.java
@@ -1,4 +1,22 @@
 package com.sau.dti.method;
 
 public class MyClassMet01 {
+    public static void showHi() {
+        System.out.println("Hi...");
+    }
+
+    public static void ShowHello() {
+        System.out.println("Hello...");
+    }
+
+    public static void main(String[] args) {
+        showHi();
+        showHi();
+        showHey();
+    }
+
+    public static void showHey() {
+        System.out.println("Hey...");
+        showHi();
+    }
 }
